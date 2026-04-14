fa427db06e05487b1993f91b5ec1da9b688cdc72
Cristian Peña <cristiandpm.2004@gmail.com>
2026-04-14 14:37:25 -0500

    feat(api): add EventRepository extending BaseRepository

---
+++ api/src/event/repositories/Event.repository.ts
@@ -0,0 +1,12 @@
+import { Repository } from "../../core/decorators/repository.decorator.js";
+import BaseRepository from "../../core/orm/repository/Base.repository.js";
+import Event from "../entities/Event.entity.js";
+
+@Repository()
+export class EventRepository extends BaseRepository<Event>{
+    constructor(){
+        super(Event)
+    }
+}
+
+export default EventRepository
