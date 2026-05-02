164dfd65128fbecc7b425780a38248d719713b10
Cristian <cmbarrera2242@gmail.com>
2026-05-02 11:15:04 -0500

    feat(api): Add ESLint config for API using TypeScript

---
+++ api/eslint.config.js
@@ -0,0 +1,33 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    ignores: ["dist/**", "node_modules/**"],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          "argsIgnorePattern": "^_",
+          "varsIgnorePattern": "^_",
+          "caughtErrorsIgnorePattern": "^_"
+        }
+      ],
+      '@typescript-eslint/no-unsafe-function-type': 'off'
+    },
+  },
+  
+);
