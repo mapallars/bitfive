1ecf67c8ec85b8e56d2e0d1ed7e2e19ed618ca70
Martín Pallares <202194519+mapallars@users.noreply.github.com>
2026-05-26 14:18:28 -0500

    hotfix: update env read on frontend api

--- app/src/core/config/api.config.mjs
+++ app/src/core/config/api.config.mjs
@@ -2,9 +2,9 @@ const ENV = import.meta.env;
 
 export const IS_DEV_MODE = ENV.VITE_IS_DEV_MODE === 'true';
 
-export const URL_BASE = 'http://localhost:3000/api/v1'
-
-console.log("soy cristian")
+export const URL_BASE = IS_DEV_MODE
+    ? ENV.VITE_API_URL_DEV
+    : ENV.VITE_API_URL_PROD;
 
 class EndpointGroup {
     constructor(base = '', paths = {}) {
